acf2e43c0880bd3aaa3a2ee2a17e9f590ac84d72
SamuelLeiva <leiva20152@gmail.com>
2024-10-20 13:59:22 -0500

    Added docker files

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerCustomOptions, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
-import { CropModule } from './crop/crop.module';
-import { WeatherModule } from './weather/weather.module';
-import { AlertModule } from './alert/alert.module';
-import { UserModule } from './user/user.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,33 +11,7 @@ async function bootstrap() {
     }
   ));
 
-  /*const config = new DocumentBuilder()
-    .setTitle('API Agricola')
-    .setDescription('Descripción de la API')
-    .setVersion('1.0')
-    .addTag('agricola')
-    .build();
-
-  const options: SwaggerDocumentOptions = {
-    include: [
-      AlertModule,
-      CropModule,
-      UserModule,
-      WeatherModule
-    ],
-    operationIdFactory: (
-      controllerKey: string,
-      methodKey: string
-    ) => methodKey
-  };
-
-  const document = SwaggerModule.createDocument(app, config, options);
-
-  const setupOptions: SwaggerCustomOptions = {
-    yamlDocumentUrl: "api-yaml"
-  }
-
-  SwaggerModule.setup('api', app, document, setupOptions);*/
+  app.use(helmet())
 
   await app.listen(3000);
 }
